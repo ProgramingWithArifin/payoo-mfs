199f8ff377f50f20cbab7ddc6934df44e14a4aa2
Arifin Mozumder <100036799+ProgramingWithArifin@users.noreply.github.com>
2026-02-19 22:12:14 +0600

    location fix

--- js/login.js
+++ js/login.js
@@ -7,7 +7,7 @@ document.getElementById("login-btn").addEventListener("click", function(){
     let inputPin = pin.value;
 // validate number and pin
     if(inputNum === "01234567890" && inputPin ==="1234"){
-        window.location.assign("/home.html")
+        window.location.assign("home.html")
     }
     else{
         alert("Enter a valid number and pin");
