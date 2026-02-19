65e5b08aa99028893aa69109e0714e881d34b83d
Arifin Mozumder <100036799+ProgramingWithArifin@users.noreply.github.com>
2026-02-19 14:54:55 +0600

    login page done

---
+++ js/login.js
@@ -0,0 +1,16 @@
+document.getElementById("login-btn").addEventListener("click", function(){
+// number input 
+    let num = document.getElementById("number")
+    let inputNum = num.value;
+// pin input    
+    let pin = document.getElementById("pin")
+    let inputPin = pin.value;
+// validate number and pin
+    if(inputNum === "01234567890" && inputPin ==="1234"){
+        window.location.assign("/home.html")
+    }
+    else{
+        alert("Enter a valid number and pin");
+        return;
+    }
+})
