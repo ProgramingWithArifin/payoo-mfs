1a3bec128a0f68558b9566d7b71d3c127cd2f479
Arifin Mozumder <100036799+ProgramingWithArifin@users.noreply.github.com>
2026-02-20 07:42:22 +0600

    add money section done

--- js/cashout.js
+++ js/cashout.js
@@ -2,7 +2,7 @@ document.getElementById("cashout-btn").addEventListener("click", function(){
     let agentNumber =valueFromId("agent-number");
     let balance = getBalance();
     let amount = Number(valueFromId("cashout-amount"));
-    let pin = valueFromId("pin");
+    let pin = valueFromId("cashout-pin");
 if(agentNumber.length === 11 && agentNumber.startsWith("01") && !isNaN(agentNumber) && pin ==="1234") {
         if(balance>=amount && amount>0){
             let newBalance = balance-amount;
